e95724e5091aa1eb2fd840c436ff114034be2d9d
chakerz <chakib.meziani@gmail.com>
2019-12-10 13:57:20 +0100

    Jenkins Trigger Test 3

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -65,10 +65,10 @@ public class MyFirstTest {
         if(title.equals("Secure Area")){
             driver.findElement(By.className("radius")).click();
             assertThat(title, is("Secure Area"));
-            return "Valid Userrx";
+            return "Valid User";
         }
 
-        return "Invalid Userrx";
+        return "Invalid User";
     }
 
 
